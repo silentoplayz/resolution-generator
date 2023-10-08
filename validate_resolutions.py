4b4e99532141c4a3b3311775e1adead352767700
Silentoplayz <50341825+Silentoplayz@users.noreply.github.com>
2023-10-08 17:05:39 -0400

    Add files via upload
    ### Changelog:
    
    #### 🔄 Function Modifications:
    - **`get_integer_input`**:
      - Introduced a `default_value` parameter.
      - The function now returns the `default_value` if the user input is empty and a default is provided.
      
    - **`get_aspect_ratio_input`**:
      - Introduced a `default_value` parameter.
      - The function now returns the `default_value` if the user input is empty.
    
    - **`display_and_save_results`**:
      - Enhanced resolution output to include:
        - Decimal representation of the aspect ratio.
        - Formatted pixel count with commas.
    
    #### ✨ New Features:
    - Introduced two new helper functions:
      - **`format_float(value)`**: Provides a cleaner float representation by trimming unnecessary zeros.
      - **`format_pixel_count(value)`**: Formats numbers with commas for better readability.
    
    #### 🛠 Default Values:
    - **`get_user_input`**:
      - Added default values for user prompts:
        - Starting resolution: 512
        - Ending resolution: 2048
        - Increment/multiple: 64
        - Maximum Pixel Limit: 1048576
        - Aspect Ratio Filter: (empty string)
    
    #### 🌐 General:
    - Maintained the core structure and flow of the program.

--- validate_resolutions.py
+++ validate_resolutions.py
@@ -3,11 +3,14 @@ import math
 valid_resolutions = []
 invalid_resolutions = []
 
-def get_integer_input(prompt, min_value=None, max_value=None):
+def get_integer_input(prompt, min_value=None, max_value=None, default_value=None):
     """Helper function to get integer input from the user."""
     while True:
+        value_str = input(prompt)
+        if not value_str and default_value is not None:
+            return default_value
         try:
-            value = int(input(prompt))
+            value = int(value_str)
             if (min_value is not None and value < min_value) or (max_value is not None and value > max_value):
                 print(f"Please enter a value between {min_value} and {max_value}.")
                 continue
@@ -15,12 +18,12 @@ def get_integer_input(prompt, min_value=None, max_value=None):
         except ValueError:
             print("Please enter a valid integer.")
 
-def get_aspect_ratio_input(prompt):
+def get_aspect_ratio_input(prompt, default_value=""):
     """Helper function to get a valid aspect ratio input from the user."""
     while True:
         ratio = input(prompt)
         if not ratio:
-            return ""
+            return default_value
         parts = ratio.split(":")
         if len(parts) != 2 or not parts[0].isdigit() or not parts[1].isdigit():
             print("Please enter a valid aspect ratio format (e.g., 16:9) or leave blank.")
@@ -36,6 +39,15 @@ def aspect_ratio_to_float(ratio):
     """Converts an aspect ratio string (e.g., '16:9') to a float (e.g., 1.7777)."""
     width, height = map(int, ratio.split(':'))
     return width / height
+
+def format_float(value):
+    """Formats a float to a string, removing unnecessary trailing zeros and decimal points."""
+    str_value = "{:.4f}".format(value)
+    return str_value.rstrip('0').rstrip('.') if '.' in str_value else str_value
+
+def format_pixel_count(value):
+    """Formats a number with commas."""
+    return "{:,}".format(value)
 
 def populate_resolutions():
     global valid_resolutions, invalid_resolutions
@@ -109,15 +121,19 @@ def display_and_save_results():
         
         file.write("Valid Resolutions:\n")
         for resolution in valid_resolutions:
-            aspect_ratio = get_aspect_ratio(resolution[0], resolution[1])
+            aspect_ratio_str = get_aspect_ratio(resolution[0], resolution[1])
+            aspect_ratio_float = format_float(aspect_ratio_to_float(aspect_ratio_str))
             total_pixels = resolution[0] * resolution[1]
-            file.write(f"{resolution[0]}x{resolution[1]} ({aspect_ratio}, {total_pixels} pixels)\n")
+            formatted_pixels = format_pixel_count(total_pixels)
+            file.write(f"Resolution: {resolution[0]}x{resolution[1]}, Aspect Ratio: {aspect_ratio_str}, Decimal: {aspect_ratio_float}, Pixels: {formatted_pixels}\n")
         
         file.write("\nInvalid Resolutions:\n")
         for resolution in invalid_resolutions:
-            aspect_ratio = get_aspect_ratio(resolution[0], resolution[1])
+            aspect_ratio_str = get_aspect_ratio(resolution[0], resolution[1])
+            aspect_ratio_float = format_float(aspect_ratio_to_float(aspect_ratio_str))
             total_pixels = resolution[0] * resolution[1]
-            file.write(f"{resolution[0]}x{resolution[1]} ({aspect_ratio}, {total_pixels} pixels)\n")
+            formatted_pixels = format_pixel_count(total_pixels)
+            file.write(f"Resolution: {resolution[0]}x{resolution[1]}, Aspect Ratio: {aspect_ratio_str}, Decimal: {aspect_ratio_float}, Pixels: {formatted_pixels}\n")
     print("Results saved to resolutions.txt")
 
 def main_menu():
@@ -245,11 +261,11 @@ def aspect_ratio_menu():
 
 def get_user_input():
     global start_resolution, end_resolution, increment, max_pixel_limit, filter_aspect_ratio
-    start_resolution = get_integer_input("Enter the starting resolution (e.g., 512): ")
-    end_resolution = get_integer_input("Enter the ending resolution (e.g., 2048): ", min_value=start_resolution)
-    increment = get_integer_input("Enter the increment/multiple (e.g., 64): ")
-    max_pixel_limit = get_integer_input("Enter the Maximum Pixel Limit (e.g., 1048576): ")
-    filter_aspect_ratio = get_aspect_ratio_input("Enter a specific aspect ratio to filter by (e.g., 16:9) or leave blank: ")
+    start_resolution = get_integer_input("Enter the starting resolution (e.g., 512): ", default_value=512)
+    end_resolution = get_integer_input("Enter the ending resolution (e.g., 2048): ", min_value=start_resolution, default_value=2048)
+    increment = get_integer_input("Enter the increment/multiple (e.g., 64): ", default_value=64)
+    max_pixel_limit = get_integer_input("Enter the Maximum Pixel Limit (e.g., 1048576): ", default_value=1048576)
+    filter_aspect_ratio = get_aspect_ratio_input("Enter a specific aspect ratio to filter by (e.g., 16:9) or leave blank: ", default_value="")
     populate_resolutions()
 
 if __name__ == "__main__":
