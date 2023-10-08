954f9599e1af51ed46ab6501a2fa9b5457f492cf
Silentoplayz <50341825+Silentoplayz@users.noreply.github.com>
2023-10-08 18:39:16 -0400

    Add files via upload
    ### Added
    - New function `get_orientation(width, height)` to determine the orientation of a resolution.
    - New function `is_within_aspect_ratio_range(width, height, min_ratio, max_ratio)` to check if a resolution is within a specified aspect ratio range.
    - New user inputs in `get_user_input()` for:
      - Minimum pixel limit
      - Orientation filter
      - Aspect ratio range
    
    ### Changed
    - Modified `populate_resolutions()` to:
      - Handle unique resolutions using sets.
      - Introduce aspect ratio range filtering.
      - Adjust the loop for height to start from width to avoid duplicates.
      - Add orientation and aspect ratio range checks in the filtering logic.
    - Updated `sort_and_display(method, order)` to include a new sorting method `aspect_ratio_decimal` for sorting by the decimal value of the aspect ratio.
    - Enhanced `display_and_save_results()` to include orientation information in the saved results.
    - Expanded `aspect_ratio_menu()` with options to sort by the decimal value of the aspect ratio.
    
    ### General Improvements
    - Resolutions can now be filtered by orientation (Horizontal, Vertical, Square).
    - Introduced the ability to specify a range for aspect ratios to filter resolutions.
    - Resolutions can be sorted based on their aspect ratio's decimal value.
    - Both minimum and maximum pixel limits are considered when filtering resolutions.

--- validate_resolutions.py
+++ validate_resolutions.py
@@ -35,6 +35,15 @@ def get_aspect_ratio(width, height):
     gcd = math.gcd(width, height)
     return f"{width//gcd}:{height//gcd}"
 
+def get_orientation(width, height):
+    """Determines the orientation of a resolution based on its width and height."""
+    if width > height:
+        return "Horizontal"
+    elif width < height:
+        return "Vertical"
+    else:
+        return "Square"
+
 def aspect_ratio_to_float(ratio):
     """Converts an aspect ratio string (e.g., '16:9') to a float (e.g., 1.7777)."""
     width, height = map(int, ratio.split(':'))
@@ -49,22 +58,38 @@ def format_pixel_count(value):
     """Formats a number with commas."""
     return "{:,}".format(value)
 
+def is_within_aspect_ratio_range(width, height, min_ratio, max_ratio):
+    """Check if the given width and height are within the specified aspect ratio range."""
+    current_ratio = aspect_ratio_to_float(get_aspect_ratio(width, height))
+    return min_ratio <= current_ratio <= max_ratio
+
 def populate_resolutions():
     global valid_resolutions, invalid_resolutions
-    valid_resolutions.clear()
-    invalid_resolutions.clear()
+    valid_resolutions_set = set()
+    invalid_resolutions_set = set()
+
+    min_ratio, max_ratio = 0, float('inf')  # Default values
+    if aspect_ratio_range:
+        min_ratio, max_ratio = map(aspect_ratio_to_float, aspect_ratio_range.split('-'))
+
     for width in range(start_resolution, end_resolution + 1, increment):
-        for height in range(start_resolution, end_resolution + 1, increment):
-            if width * height <= max_pixel_limit:
-                aspect_ratio = get_aspect_ratio(width, height)
-                if not filter_aspect_ratio or aspect_ratio == filter_aspect_ratio:
-                    valid_resolutions.append((width, height))
-                    if not filter_aspect_ratio or aspect_ratio == get_aspect_ratio(height, width):
-                        valid_resolutions.append((height, width))
+        for height in range(width, end_resolution + 1, increment):  # Start from width to avoid duplicates
+            aspect_ratio = get_aspect_ratio(width, height)
+            orientation = get_orientation(width, height)
+            if width * height <= max_pixel_limit and width * height >= min_pixel_limit:
+                if (not filter_aspect_ratio or aspect_ratio == filter_aspect_ratio) and \
+                   (not orientation_filter or orientation == orientation_filter) and \
+                   is_within_aspect_ratio_range(width, height, min_ratio, max_ratio):
+                    valid_resolutions_set.add((width, height))
+                    if width != height:
+                        valid_resolutions_set.add((height, width))
             else:
-                invalid_resolutions.append((width, height))
-                invalid_resolutions.append((height, width))
-    valid_resolutions = list(set(valid_resolutions))
+                invalid_resolutions_set.add((width, height))
+                if width != height:
+                    invalid_resolutions_set.add((height, width))
+
+    valid_resolutions = list(valid_resolutions_set)
+    invalid_resolutions = list(invalid_resolutions_set)
 
 def sort_and_display(method, order):
     global valid_resolutions, invalid_resolutions
@@ -106,6 +131,13 @@ def sort_and_display(method, order):
         else:
             valid_resolutions.sort(key=lambda x: aspect_ratio_to_float(get_aspect_ratio(x[0], x[1])), reverse=True)
             invalid_resolutions.sort(key=lambda x: aspect_ratio_to_float(get_aspect_ratio(x[0], x[1])), reverse=True)
+    elif method == "aspect_ratio_decimal":
+        if order == "asc":
+            valid_resolutions.sort(key=lambda x: aspect_ratio_to_float(get_aspect_ratio(x[0], x[1])))
+            invalid_resolutions.sort(key=lambda x: aspect_ratio_to_float(get_aspect_ratio(x[0], x[1])))
+        else:
+            valid_resolutions.sort(key=lambda x: aspect_ratio_to_float(get_aspect_ratio(x[0], x[1])), reverse=True)
+            invalid_resolutions.sort(key=lambda x: aspect_ratio_to_float(get_aspect_ratio(x[0], x[1])), reverse=True)
     display_and_save_results()
 
 def display_and_save_results():
@@ -125,15 +157,17 @@ def display_and_save_results():
             aspect_ratio_float = format_float(aspect_ratio_to_float(aspect_ratio_str))
             total_pixels = resolution[0] * resolution[1]
             formatted_pixels = format_pixel_count(total_pixels)
-            file.write(f"Resolution: {resolution[0]}x{resolution[1]}, Aspect Ratio: {aspect_ratio_str}, Decimal: {aspect_ratio_float}, Pixels: {formatted_pixels}\n")
+            orientation = get_orientation(resolution[0], resolution[1])
+            file.write(f"Resolution: {resolution[0]}x{resolution[1]}, Aspect Ratio: {aspect_ratio_str} ({orientation}), Decimal: {aspect_ratio_float}, Pixels: {formatted_pixels}\n")
         
-        file.write("\nInvalid Resolutions:\n")
+        file.write("\nInvalid Resolutions (Exceeding Maximum Pixel Limit):\n")
         for resolution in invalid_resolutions:
             aspect_ratio_str = get_aspect_ratio(resolution[0], resolution[1])
             aspect_ratio_float = format_float(aspect_ratio_to_float(aspect_ratio_str))
             total_pixels = resolution[0] * resolution[1]
             formatted_pixels = format_pixel_count(total_pixels)
-            file.write(f"Resolution: {resolution[0]}x{resolution[1]}, Aspect Ratio: {aspect_ratio_str}, Decimal: {aspect_ratio_float}, Pixels: {formatted_pixels}\n")
+            orientation = get_orientation(resolution[0], resolution[1])
+            file.write(f"Resolution: {resolution[0]}x{resolution[1]}, Aspect Ratio: {aspect_ratio_str} ({orientation}), Decimal: {aspect_ratio_float}, Pixels: {formatted_pixels}\n")
     print("Results saved to resolutions.txt")
 
 def main_menu():
@@ -241,31 +275,37 @@ def aspect_ratio_menu():
     while True:
         print("\nSort by aspect ratio:")
         print("1. Closest to 1:1 first")
-        print("2. Ascending")
-        print("3. Descending")
-        print("4. Back to main menu")
+        print("2. Ascending (by ratio)")
+        print("3. Descending (by ratio)")
+        print("4. Ascending (by decimal)")
+        print("5. Descending (by decimal)")
+        print("6. Back to main menu")
         
-        choice = get_integer_input("Enter your choice: ", 1, 4)
+        choice = get_integer_input("Enter your choice: ", 1, 6)
         
         if choice == 1:
             sort_and_display("aspect_ratio", "closest")
-            break
         elif choice == 2:
             sort_and_display("aspect_ratio", "asc")
-            break
         elif choice == 3:
             sort_and_display("aspect_ratio", "desc")
-            break
         elif choice == 4:
+            sort_and_display("aspect_ratio_decimal", "asc")
+        elif choice == 5:
+            sort_and_display("aspect_ratio_decimal", "desc")
+        elif choice == 6:
             break
 
 def get_user_input():
-    global start_resolution, end_resolution, increment, max_pixel_limit, filter_aspect_ratio
+    global start_resolution, end_resolution, increment, max_pixel_limit, min_pixel_limit, filter_aspect_ratio, orientation_filter, aspect_ratio_range
     start_resolution = get_integer_input("Enter the starting resolution (e.g., 512): ", default_value=512)
     end_resolution = get_integer_input("Enter the ending resolution (e.g., 2048): ", min_value=start_resolution, default_value=2048)
     increment = get_integer_input("Enter the increment/multiple (e.g., 64): ", default_value=64)
     max_pixel_limit = get_integer_input("Enter the Maximum Pixel Limit (e.g., 1048576): ", default_value=1048576)
+    min_pixel_limit = get_integer_input("Enter the Minimum Pixel Limit (e.g., 262144): ", default_value=262144)
     filter_aspect_ratio = get_aspect_ratio_input("Enter a specific aspect ratio to filter by (e.g., 16:9) or leave blank: ", default_value="")
+    orientation_filter = input("Filter by orientation (Horizontal, Vertical, Square) or leave blank: ").capitalize()
+    aspect_ratio_range = input("Enter a custom aspect ratio range (e.g., 4:3-16:9) or leave blank: ")
     populate_resolutions()
 
 if __name__ == "__main__":
