7f73d62dfdedc4729833d8018b3845a6229e0507
Silentoplayz <50341825+Silentoplayz@users.noreply.github.com>
2023-10-09 01:56:06 -0400

    Add files via upload

--- validate_resolutions.py
+++ validate_resolutions.py
@@ -1,22 +1,20 @@
 import math
+import time
 
 valid_resolutions = []
 invalid_resolutions = []
 
-
+# Helper functions for input
 def get_integer_input(prompt, min_value=None, max_value=None, default_value=None):
     """
     Helper function to get integer input from the user.
-
     Args:
         prompt (str): The prompt message to display to the user.
         min_value (int, optional): The minimum allowed value. Defaults to None.
         max_value (int, optional): The maximum allowed value. Defaults to None.
         default_value (int, optional): The default value to return if no input is provided. Defaults to None.
-
     Returns:
         int: The integer value entered by the user.
-
     Raises:
         ValueError: If the input is not a valid integer.
     """
@@ -50,45 +48,7 @@ def get_aspect_ratio_input(prompt, default_value=""):
             continue
         return ratio
 
-
-def get_aspect_ratio(width, height):
-    """Helper function to calculate the aspect ratio."""
-    gcd = math.gcd(width, height)
-    return f"{width//gcd}:{height//gcd}"
-
-
-def get_orientation(width, height):
-    """Determines the orientation of a resolution based on its width and height."""
-    if width > height:
-        return "Horizontal"
-    elif width < height:
-        return "Vertical"
-    else:
-        return "Square"
-
-
-def aspect_ratio_to_float(ratio):
-    """Converts an aspect ratio string (e.g., '16:9') to a float (e.g., 1.7777)."""
-    width, height = map(int, ratio.split(":"))
-    return width / height
-
-
-def format_float(value):
-    """Formats a float to a string, removing unnecessary trailing zeros and decimal points."""
-    str_value = "{:.4f}".format(value)
-    return str_value.rstrip("0").rstrip(".") if "." in str_value else str_value
-
-
-def format_pixel_count(value):
-    """Formats a number with commas."""
-    return "{:,}".format(value)
-
-
-def is_within_aspect_ratio_range(width, height, min_ratio, max_ratio):
-    """Check if the given width and height are within the specified aspect ratio range."""
-    current_ratio = aspect_ratio_to_float(get_aspect_ratio(width, height))
-    return min_ratio <= current_ratio <= max_ratio
-
+# Functions for working with resolutions
 
 def populate_resolutions():
     global valid_resolutions, invalid_resolutions
@@ -124,14 +84,17 @@ def populate_resolutions():
     valid_resolutions = list(valid_resolutions_set)  # Convert set of valid resolutions to a list
     invalid_resolutions = list(invalid_resolutions_set)  # Convert set of invalid resolutions to a list
 
+def is_within_aspect_ratio_range(width, height, min_ratio, max_ratio):
+    """Check if the given width and height are within the specified aspect ratio range."""
+    current_ratio = aspect_ratio_to_float(get_aspect_ratio(width, height))
+    return min_ratio <= current_ratio <= max_ratio
+
 
 def upscale_downscale_percentage_for_sort(base_resolution, target_resolution):
     """Calculate the upscale percentage from the base resolution to the target resolution.
-
     Args:
         base_resolution (tuple): The base resolution as a tuple of width and height.
         target_resolution (tuple): The target resolution as a tuple of width and height.
-
     Returns:
         float: The percentage by which the base resolution needs to be upscaled to reach the target resolution.
     """
@@ -140,14 +103,47 @@ def upscale_downscale_percentage_for_sort(base_resolution, target_resolution):
     upscale_percent = ((target_pixels / base_pixels) * 100) - 100
     return upscale_percent
 
+# Functions for resolution details and formatting
+
+def get_aspect_ratio(width, height):
+    """Helper function to calculate the aspect ratio."""
+    gcd = math.gcd(width, height)
+    return f"{width//gcd}:{height//gcd}"
+
+
+def get_orientation(width, height):
+    """Determines the orientation of a resolution based on its width and height."""
+    if width > height:
+        return "Horizontal"
+    elif width < height:
+        return "Vertical"
+    else:
+        return "Square"
+
+
+def aspect_ratio_to_float(ratio):
+    """Converts an aspect ratio string (e.g., '16:9') to a float (e.g., 1.7777)."""
+    width, height = map(int, ratio.split(":"))
+    return width / height
+
+
+def format_float(value):
+    """Formats a float to a string, removing unnecessary trailing zeros and decimal points."""
+    str_value = "{:.4f}".format(value)
+    return str_value.rstrip("0").rstrip(".") if "." in str_value else str_value
+
+
+def format_pixel_count(value):
+    """Formats a number with commas."""
+    return "{:,}".format(value)
+
+# Functions for sorting and displaying resolutions
 
 def sort_resolutions(method, order):
     """Sort the valid and invalid resolutions based on the specified method and order.
-
     Args:
         method (str): The method to use for sorting. Can be "width", "height", "average", "pixels", "aspect_ratio", or "upscale".
         order (str): The order in which to sort. Can be "asc", "desc", or "closest".
-
     Returns:
         None
     """
@@ -164,8 +160,20 @@ def sort_resolutions(method, order):
         "upscale": lambda x: upscale_downscale_percentage_for_sort(base_res, x),  # Sort based on percentage of upscale/downscale required to reach the base resolution
     }
 
+    start_time = time.time()  # Start measuring time
+    count = 0  # Initialize count
+
     # Sort the valid resolutions based on the specified method and order
     valid_resolutions.sort(key=key_map[method], reverse=(order != "asc" and order != "closest"))
+     
+    end_time = time.time()  # Stop measuring time
+    
+    # Count the number of sorted resolutions
+    for resolution in valid_resolutions:
+        count += 1
+    
+    print(f"Sorted {count} resolutions in {end_time - start_time:.2f} seconds.")
+    print("Calculations complete.")
 
     # Sort the invalid resolutions based on the specified method and order
     invalid_resolutions.sort(key=key_map[method], reverse=(order != "asc" and order != "closest"))
@@ -173,11 +181,9 @@ def sort_resolutions(method, order):
 
 def upscale_downscale_percentage(base_resolution, target_resolution):
     """Calculate the upscale/downscale percentage from the base resolution to the target resolution.
-
     Args:
         base_resolution (tuple): The base resolution as a tuple of width and height.
         target_resolution (tuple): The target resolution as a tuple of width and height.
-
     Returns:
         tuple: A tuple containing the upscale percentage and downscale percentage.
     """
@@ -191,47 +197,52 @@ def upscale_downscale_percentage(base_resolution, target_resolution):
     return upscale_percent, downscale_percent
 
 
-def write_resolution_to_file(file, resolution, base_res, end_res):
-    # Get the aspect ratio as a string
+def get_resolution_details(resolution, base_res, end_res):
+    """Get resolution details including aspect ratio, orientation, and percentages."""
     aspect_ratio_str = get_aspect_ratio(resolution[0], resolution[1])
-    # Convert the aspect ratio string to a float
     aspect_ratio_float = format_float(aspect_ratio_to_float(aspect_ratio_str))
-    # Calculate the total number of pixels in the resolution
     total_pixels = resolution[0] * resolution[1]
-    # Format the total pixel count with commas for better readability
     formatted_pixels = format_pixel_count(total_pixels)
-    # Calculate the number of megapixels
     megapixels = total_pixels / 1_000_000
-    # Determine the orientation of the resolution (landscape or portrait)
     orientation = get_orientation(resolution[0], resolution[1])
-    # Calculate the percentage of upscale and downscale from the base resolution
     upscale_percent, downscale_percent = upscale_downscale_percentage(base_res, resolution)
+    megapixels_display = f"{megapixels:.2f} MP" if megapixels > 0 else ""
+    upscale_display = (
+        f"{int(upscale_percent)}% upscale of {base_res[0]}x{base_res[1]}"
+        if upscale_percent % 1 == 0
+        else f"{upscale_percent:.3f}% upscale of {base_res[0]}x{base_res[1]}"
+    ) if total_pixels > 1 and not (base_res == resolution or (upscale_percent == 0 and downscale_percent == 0)) else ""
+    downscale_display = (
+        f"{int(downscale_percent)}% downscale of {end_res[0]}x{end_res[1]}"
+        if downscale_percent % 1 == 0
+        else f"{downscale_percent:.3f}% downscale of {end_res[0]}x{end_res[1]}"
+    ) if total_pixels > 1 and not (base_res == resolution or (upscale_percent == 0 and downscale_percent == 0)) else ""
+    
+    return aspect_ratio_str, aspect_ratio_float, formatted_pixels, megapixels_display, orientation, upscale_display, downscale_display
 
-    # Format the upscale and downscale percentages based on their values
-    if upscale_percent % 1 == 0:  # Check if it's a whole number
-        upscale_display = f"{int(upscale_percent)}% upscale of {base_res[0]}x{base_res[1]}"
-    else:
-        upscale_display = f"{upscale_percent:.3f}% upscale of {base_res[0]}x{base_res[1]}"
-
-    if downscale_percent % 1 == 0:  # Check if it's a whole number
-        downscale_display = f"{int(downscale_percent)}% downscale of {end_res[0]}x{end_res[1]}"
-    else:
-        downscale_display = f"{downscale_percent:.3f}% downscale of {end_res[0]}x{end_res[1]}"
-
-    # Format the aspect ratio with commas for better readability
-    formatted_aspect_ratio = ":".join([format_pixel_count(int(part)) for part in aspect_ratio_str.split(":")])
-
-    # Write the formatted information to the file
+def write_resolution_to_file(file, resolution, base_res, end_res):
+    """Write resolution details to a file."""
+    aspect_ratio_str, aspect_ratio_float, formatted_pixels, megapixels_display, orientation, upscale_display, downscale_display = get_resolution_details(resolution, base_res, end_res)
+    
+    total_pixels = resolution[0] * resolution[1]  # Add this line to define total_pixels
+    
+    megapixels_part = f"| {megapixels_display}" if megapixels_display else ""
+    upscale_part = f"| {upscale_display}" if upscale_display else ""
+    downscale_part = f"| {downscale_display}" if downscale_display else ""
+    
     file.write(
-        f"Resolution: {resolution[0]}x{resolution[1]} | Aspect Ratio: {formatted_aspect_ratio} ({orientation}) | Aspect Ratio Decimal: {aspect_ratio_float} | {formatted_pixels} Pixels ({megapixels:.2f} MP) | {upscale_display.rstrip('0').rstrip('.')} | {downscale_display.rstrip('0').rstrip('.')}\n"
+        f"Resolution: {resolution[0]}x{resolution[1]} | Aspect Ratio: {aspect_ratio_str} | {orientation} | Aspect Ratio Decimal: {aspect_ratio_float} | {formatted_pixels} Pixel{'s' if total_pixels != 1 else ''} {megapixels_part} {upscale_part} {downscale_part}\n"
     )
 
+# Functions for menu and user interactions
 
 def display_and_save_results():
     # Set the base and end resolutions
     base_res = (start_resolution, start_resolution)
     end_res = (end_resolution, end_resolution)
-    
+
+    start_time = time.time()  # Start measuring time
+
     # Open the file in write mode
     with open("resolutions.txt", "w") as file:
         # Write the constraints used section
@@ -253,6 +264,9 @@ def display_and_save_results():
         file.write("\nInvalid Resolutions (Exceeding Maximum Pixel Limit):\n")
         for resolution in invalid_resolutions:
             write_resolution_to_file(file, resolution, base_res, end_res)
+            end_time = time.time()  # Stop measuring time
+
+    print(f"Saved results to 'resolutions.txt' in {end_time - start_time:.2f} seconds.")
 
 
 def menu(title, options, actions):
@@ -337,9 +351,9 @@ def print_saved_message():
     print("Results have been saved to resolutions.txt!")
 
 
-def get_user_input():
-    # Get user input for various parameters
-    global start_resolution, end_resolution, increment, max_pixel_limit, min_pixel_limit, filter_aspect_ratio, orientation_filter, aspect_ratio_range
+def collect_resolution_parameters():
+    """Collect resolution-related parameters from the user."""
+    global start_resolution, end_resolution, increment, max_pixel_limit, min_pixel_limit
     start_resolution = get_integer_input(
         "Enter the starting resolution (e.g., 512): ", default_value=512
     )
@@ -357,14 +371,25 @@ def get_user_input():
     min_pixel_limit = get_integer_input(
         "Enter the Minimum Pixel Limit (e.g., 262144): ", default_value=262144
     )
+
+def collect_aspect_ratio_filter():
+    """Collect aspect ratio filter from the user."""
+    global filter_aspect_ratio
     filter_aspect_ratio = get_aspect_ratio_input(
         "Enter a specific aspect ratio to filter by (e.g., 16:9) or leave blank: ",
         default_value="",
     )
+
+def collect_orientation_filter():
+    """Collect orientation filter from the user."""
+    global orientation_filter
     orientation_filter = input(
         "Filter by orientation (Horizontal, Vertical, Square) or leave blank: "
     ).capitalize()
 
+def collect_aspect_ratio_range():
+    """Collect custom aspect ratio range from the user."""
+    global aspect_ratio_range
     while True:
         aspect_ratio_range = input(
             "Enter a custom aspect ratio range (e.g., 4:3-16:9) or leave blank: "
@@ -373,9 +398,15 @@ def get_user_input():
             break
         print("Invalid aspect ratio range format. Please use the format '4:3-16:9'.")
 
+def get_user_input():
+    """Get user input for various parameters."""
+    collect_resolution_parameters()
+    collect_aspect_ratio_filter()
+    collect_orientation_filter()
+    collect_aspect_ratio_range()
+    
     # Populate the resolutions list based on user input
     populate_resolutions()
-
 
 if __name__ == "__main__":
     # Get user input and display the main menu
